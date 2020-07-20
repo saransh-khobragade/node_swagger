e890b28f2165486e984f3496bd8ea7cbfe1a281e
Saransh <saransh98@gmail.com>
2020-07-20 08:34:30 +0530

    done

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require('express')
 const app = express()
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
 
 app.get('/ping', async (req, res) => {
     res.status(200).send({ "ping": "pong" })
 });
+
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        title: 'test application',
+        version: '1.0.0',
+        description: 'Endpoints to the APIs',
+    },
+    host: 'http://localhost:8080/',
+    basePath: '/',
+    servers: [
+        {
+            url: 'http://localhost:8080/'
+        }
+    ]
+};
+const options = {
+    swaggerDefinition,
+    apis: [
+        "./index.js",
+        "./swagger_def/*.yaml"
+    ],
+};
+const swaggerSpec = swaggerJsDoc(options);
+app.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+    console.log("swaggerSpec", swaggerSpec)
+});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
 
 var server = require('http').createServer(app);
 
